214863d20546acfd484cbe7b749b65503acbf521
Darkborn <darkandrai1987@gmail.com>
2024-07-06 14:46:28 -0400

    Change

--- lua/entities/npc_vj_zps_zinf_ply/shared.lua
+++ lua/entities/npc_vj_zps_zinf_ply/shared.lua
@@ -8,10 +8,10 @@ ENT.Instructions    = "Click on the spawnicon to spawn it."
 ENT.Category        = "Zombie Panic! Source"
 
 if (CLIENT) then
-local Name = "Zombie (Player)"
-local LangName = "npc_vj_zps_zinf_ply"
-language.Add(LangName, Name)
-killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
-language.Add("#"..LangName, Name)
-killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
+    local Name = "Zombie (Player)"
+    local LangName = "npc_vj_zps_zinf_ply"
+    language.Add(LangName, Name)
+    killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
+    language.Add("#"..LangName, Name)
+    killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
 end
