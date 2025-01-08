9f4b09206adadbb2589fd3fa2bd5df08eed9928b
Darkborn <darkandrai1987@gmail.com>
2025-01-08 13:39:11 -0500

    IED update

--- lua/entities/obj_vj_zps_ied.lua
+++ lua/entities/obj_vj_zps_ied.lua
@@ -45,23 +45,27 @@ function ENT:InitPhys()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:OnThink()
-    if !IsValid(self:GetOwner()) && self.IED_Armed then self:Detonate() return end
-    if !self.IED_Armed then return end
-    if self.IED_Armed && CurTime() > self.IED_NextBlinkT then
+ if !IsValid(self:GetOwner()) && self.IED_Armed then self:Detonate() return end
+ if !self.IED_Armed then return end
+ if self.IED_Armed && CurTime() > self.IED_NextBlinkT then
+ if self:GetSkin() == 0 then
     self:SetSkin(1)
+ elseif self:GetSkin() == 1 then
+    self:SetSkin(0)
+end
+    /*self:SetSkin(1)
     timer.Simple(0.2, function() if IsValid(self) then self:SetSkin(1) end end)
-    timer.Simple(0.4, function() if IsValid(self) then self:SetSkin(0) end end)
+    timer.Simple(0.4, function() if IsValid(self) then self:SetSkin(0) end end)*/ -- Old code
     VJ.CreateSound(self,"darkborn/zps/weapons/explosives/ied/ied_beep_armed.wav",75,100)
     self.IED_NextBlinkT = CurTime() + 1
 end
- if IsValid(self:GetOwner()) then
+    if IsValid(self:GetOwner()) then
     local owner = self:GetOwner()
     local ownerDist = self:GetPos():Distance(owner:GetPos())
     if IsValid(owner) && ownerDist <= 500 && self:Visible(owner) then
     for _,v in ipairs(ents.FindInSphere(self:GetPos(),150)) do
-        if IsValid(v) && IsValid(owner) && (v != owner.VJ_TheController && v != owner.VJ_TheControllerBullseye) && ((v:IsNPC() && v:GetClass() != owner:GetClass() && (owner:IsPlayer() or (owner:IsNPC() && owner:Disposition(v) != D_LI && !v:IsFlagSet(FL_NOTARGET)))) or (v:IsPlayer() && v:Alive() && owner:Disposition(v) != D_LI && (owner:IsPlayer() or (!VJ_CVAR_IGNOREPLAYERS && !v:IsFlagSet(FL_NOTARGET))))) then
-            self:Detonate() end
-            end
+    if IsValid(v) && IsValid(owner) && (v != owner.VJ_TheController && v != owner.VJ_TheControllerBullseye) && ((v:IsNPC() && v:GetClass() != owner:GetClass() && (owner:IsPlayer() or (owner:IsNPC() && owner:Disposition(v) != D_LI && !v:IsFlagSet(FL_NOTARGET)))) or (v:IsPlayer() && v:Alive() && owner:Disposition(v) != D_LI && (owner:IsPlayer() or (!VJ_CVAR_IGNOREPLAYERS && !v:IsFlagSet(FL_NOTARGET))))) then
+        self:Detonate() end end
         end
     end
 end
@@ -71,39 +75,39 @@ local vecZ4 = Vector(0, 0, 4)
 local vezZ100 = Vector(0, 0, 100)
 --
 function ENT:Detonate()
-    self.VJ_ID_Danger = true
-    self:SetSkin(1)
-    VJ.CreateSound(self,"darkborn/zps/weapons/explosives/ied/ied_alarm.wav",75,100)
-    timer.Simple(SoundDuration("darkborn/zps/weapons/explosives/ied/ied_alarm.wav"),function() if IsValid(self) then
-    local myPos = self:GetPos()
+        self.VJ_ID_Danger = true
+        self:SetSkin(1)
+        VJ.CreateSound(self,"darkborn/zps/weapons/explosives/ied/ied_alarm.wav",75,100)
+        timer.Simple(SoundDuration("darkborn/zps/weapons/explosives/ied/ied_alarm.wav"),function() if IsValid(self) then
+        local myPos = self:GetPos()
 
-    ParticleEffect("vj_zps_IED", myPos, defAngle, nil)
-    VJ.EmitSound(self,"VJ.ZPS_IED_Explosion")
-    util.ScreenShake(myPos, 100, 200, 1, 2500)
+        ParticleEffect("vj_zps_IED", myPos, defAngle, nil)
+        VJ.EmitSound(self,"VJ.ZPS_IED_Explosion")
+        util.ScreenShake(myPos, 100, 200, 1, 2500)
 
-    local expLight = ents.Create("light_dynamic")
-    expLight:SetKeyValue("brightness", "4")
-    expLight:SetKeyValue("distance", "300")
-    expLight:SetLocalPos(myPos)
-    expLight:SetLocalAngles(self:GetAngles())
-    expLight:Fire("Color", "255 150 0")
-    //expLight:SetParent(self)
-    expLight:Spawn()
-    expLight:Activate()
-    expLight:Fire("TurnOn", "", 0)
-    expLight:Fire("Kill","",0.08)
-    //self:DeleteOnRemove(expLight)
+        local expLight = ents.Create("light_dynamic")
+        expLight:SetKeyValue("brightness", "4")
+        expLight:SetKeyValue("distance", "300")
+        expLight:SetLocalPos(myPos)
+        expLight:SetLocalAngles(self:GetAngles())
+        expLight:Fire("Color", "255 150 0")
+        //expLight:SetParent(self)
+        expLight:Spawn()
+        expLight:Activate()
+        expLight:Fire("TurnOn", "", 0)
+        expLight:Fire("Kill","",0.08)
+        //self:DeleteOnRemove(expLight)
 
-    self:SetLocalPos(myPos + vecZ4) -- Because the entity is too close to the ground
-    local tr = util.TraceLine({
-        start = myPos,
-        endpos = myPos - vezZ100,
-        filter = self
-    })
-    util.Decal(VJ.PICK(self.CollisionDecal), tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
+        self:SetLocalPos(myPos + vecZ4) -- Because the entity is too close to the ground
+        local tr = util.TraceLine({
+            start = myPos,
+            endpos = myPos - vezZ100,
+            filter = self
+        })
+        util.Decal(VJ.PICK(self.CollisionDecal), tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
 
-    self:DealDamage()
-    self:Remove()
+        self:DealDamage()
+        self:Remove()
         end
     end)
 end
