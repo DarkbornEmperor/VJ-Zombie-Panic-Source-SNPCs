de0007cd0a3e4982bea98c4c39fc5a3d1fc1c82a
Darkborn <darkandrai1987@gmail.com>
2026-03-13 02:01:10 -0400

    Rare bug fix

--- lua/weapons/weapon_vj_zps_glock18c.lua
+++ lua/weapons/weapon_vj_zps_glock18c.lua
@@ -65,6 +65,7 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:NPC_Reload()
     local owner = self:GetOwner()
+    if !IsValid(owner) then return end
     owner.NextChaseTime = 0
     self.BaseClass.NPC_Reload(self)
 end
